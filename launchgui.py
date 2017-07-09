eabca85b2c7a851376c0f779ba6809b5eb75f978
billyg1125@gmail.com <billyg1125@gmail.com>
2017-07-08 22:32:01 -0400

    Created basic GUI Launcher, created legacy folder, revamped dependency installation

---
+++ launchgui.py
@@ -0,0 +1,185 @@
+import sys
+import os
+import platform
+import pip
+import time
+import json
+import subprocess
+import zipfile
+from tkinter import *
+from tkinter import messagebox
+try:
+    import wget
+except ImportError:
+    print('wget currently not installed. Please run option 3 or 4.')
+    time.sleep(2)
+
+useros = platform.system()
+
+if useros == 'Windows':
+    clear = 'cls'
+    os.system('title Input')
+elif useros == 'Linux':
+    clear = 'clear'
+else:
+    clear = 'clear'
+
+def install(package):
+    pip.main(['install', package])
+
+class Launcher(Frame):
+
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
+        self.master = master
+        self.init_window()
+
+    def init_window(self):
+        self.master.title('Osmibot Launcher')
+        self.pack(fill=BOTH, expand=1)
+        menu = Menu(self.master)
+        self.master.config(menu=menu)
+
+        file = Menu(menu)
+        file.add_command(label='Exit', command=self.client_exit)
+        menu.add_cascade(label='File', menu=file)
+
+        setup = Menu(menu)
+        setup.add_command(label='Configure Settings', command=self.config)
+        setup.add_command(label='Download Bot', command=self.download)
+        menu.add_cascade(label='Setup', menu=setup)
+
+        deps = Menu(menu)
+        deps.add_command(label='Install Basic Dependencies', command=self.basic)
+        deps.add_command(label='Install All Dependencies', command=self.alldeps)
+        menu.add_cascade(label='Dependencies', menu=deps)
+
+        osmi = Label(self.master, text="Osmibot", font=("Arial", 36))
+        osmi.place(x=100, y=75)
+
+        start = Button(self.master, text='Start Bot', command=self.start)
+        start.place(x=160, y=150)
+
+    def client_exit(self):
+        exit()
+
+    def start(self):
+        try:
+            os.chdir('master')
+            if useros == 'Windows':
+                os.system('python osmibot.py')
+            else:
+                os.system('python3 osmibot.py')
+            os.chdir('..')
+        except Exception:
+            print('Bot currently not downloaded. Install dependencies and use option 6.')
+            time.sleep(2)
+        os.system(clear)
+
+    def config(self):
+        messagebox.showinfo('Config', 'Config opened in console.')
+        os.system(clear)
+        tokendump = input("Enter your bot's token here: ")
+        os.system(clear)
+        clientdump = input("Enter your bot's client secret here: ")
+        os.system(clear)
+        ownerdump = input("Enter your user id here: ")
+        os.system(clear)
+        serverdump = input("Enter your server id here: ")
+        os.system(clear)
+        moddump = input("Enter the name of your mod role here: ")
+        os.system(clear)
+        admindump = input("Enter the name of your admin role here: ")
+        os.system(clear)
+        data = {
+            "token":tokendump,
+            "client":clientdump
+        }
+        perms = {
+            "ownerid":ownerdump,
+            "serverid":serverdump,
+            "modrole":moddump,
+            "adminrole":admindump
+        }
+        try:
+            os.chdir('master')
+        except Exception:
+            messagebox.showinfo('Error', 'Master directory does not exist. Download Bot first.')
+        with open('config.json', 'w') as f:
+            json.dump(data, f)
+        with open('permissions.json', 'w') as p:
+            json.dump(perms, p)
+        os.chdir('..')
+        time.sleep(1)
+        os.system(clear)
+
+    def basic(self):
+        messagebox.showinfo('Basic Dependencies', 'Installing Basic Dependencies in Console')
+        os.system(clear)
+        print('The following operations require sudo on linux.')
+        print('If the launcher is not being run as sudo, exit now.')
+        input('Press enter to continue.')
+        os.system(clear)
+        print('Installing discord.py, aiofiles, wget and requirements...')
+        install('discord.py')
+        install('aiofiles')
+        install('wget')
+        print('Installed dependencies!')
+        time.sleep(3)
+        os.system(clear)
+
+    def alldeps(self):
+        messagebox.showinfo('All Dependencies', 'Installing All Dependencies in Console')
+        os.system(clear)
+        print('The following operations require administrator access.')
+        print('If you are not running in an administrative terminal/sudo, exit the program now.')
+        input('Press enter to continue.')
+        os.system(clear)
+        print('Installing discord.py, aiofiles, cffi, pycparser, PyNaCl, youtube_dl, wget and requirements...')
+        install('discord.py')
+        install('aiofiles')
+        install('PyNaCl')
+        install('youtube_dl')
+        install('wget')
+        install('cffi')
+        install('pycparser')
+        print('Installed python dependencies!')
+        time.sleep(1)
+        print('Installing ffmpeg...')
+        if useros == 'Windows':
+            messagebox.showinfo('Error', 'Windows installation of FFMPEG must be done manually.')
+        elif useros == 'Linux':
+            os.system('add-apt-repository ppa:mc3man/trusty-media')
+            os.system('apt-get update')
+            os.system('apt-get install ffmpeg')
+            os.system('apt-get install frei0r-plugins')
+        elif useros == 'Darwin':
+            print('Mac Installation requires that you currently have homebrew installed.')
+            print('If homebrew is not installed, please close the program and install it.')
+            input('Press enter to continue.')
+            os.system('brew install ffmpeg --with-fdk-aac --with-ffplay --with-freetype --with-frei0r --with-libass --with-libvo-aacenc --with-libvorbis --with-libvpx --with-opencore-amr --with-openjpeg --with-opus --with-rtmpdump --with-schroedinger --with-speex --with-theora --with-tools')
+        print('Installed all depedencies!')
+        time.sleep(3)
+        os.system(clear)
+
+    def download(self):
+        messagebox.showinfo('Downloading Bot', 'Downloading Bot in Console')
+        try:
+            wget.download('https://github.com/ANT1H3R0/osmibot/archive/master.zip', 'master.zip')
+            print('Downloaded')
+            time.sleep(2)
+            print('Unzipping...')
+            with zipfile.ZipFile('master.zip', 'r') as zip_ref:
+                zip_ref.extractall()
+            os.rename('osmibot-master', 'master')
+            os.remove('master.zip')
+        except Exception as e:
+            print('Error!')
+            print(e)
+            input('Press enter to continue...')
+        os.system(clear)
+
+root = Tk()
+root.geometry('400x300')
+app = Launcher(root)
+root.mainloop()
---
+++ legacy/launcher.py
@@ -0,0 +1,177 @@
+import sys
+import os
+import platform
+import pip
+import time
+import json
+import subprocess
+import zipfile
+import requests
+try:
+    import wget
+except ImportError:
+    print('wget currently not installed. Please run option 3 or 4.')
+    time.sleep(2)
+
+useros = platform.system()
+pythonver = sys.version_info[0]
+var = 1
+
+if useros == 'Windows':
+    clear = 'cls'
+    title = 'title Osmibot Launcher'
+elif useros == 'Linux':
+    clear = 'clear'
+    title = """PROMPT_COMMAND='echo -en "\033]0;Osmibot Launcher\a"'"""
+else:
+    clear = 'clear'
+    title = 'echo -n -e "\033]0;Osmibot Launcher\007"'
+
+def install(package):
+    pip.main(['install', package])
+
+while var == 1:
+    os.system(title)
+    os.system(clear)
+    print('Osmibot Launcher v1.0')
+    print('Select an option:')
+    print('_____________________')
+    print('1. Run Osmibot')
+    print('2. Become a whale')
+    print('3. Install basic dependencies (no sound features)')
+    print('4. Install all dependencies (recommended)')
+    print('5. Configure Bot')
+    print('6. Exit')
+    print('7. Update/Install Osmibot')
+    print('8. Launch cogloader')
+    choice = input('Enter option number and press enter: ')
+
+    if choice == '1':
+        try:
+            os.chdir('master')
+            if useros == 'Windows':
+                os.system('python osmibot.py')
+            else:
+                os.system('python3 osmibot.py')
+            os.chdir('..')
+        except Exception:
+            print('Bot currently not downloaded. Install dependencies and use option 6.')
+            time.sleep(2)
+
+    if choice == '2':
+        os.system(clear)
+        print('What did you expect would happen?')
+        time.sleep(2)
+
+    if choice == '3':
+        os.system(clear)
+        print('The following operations require sudo on linux.')
+        print('If the launcher is not being run as sudo, exit now.')
+        input('Press enter to continue.')
+        os.system(clear)
+        print('Installing discord.py, aiofiles, wget and requirements...')
+        install('discord.py')
+        install('aiofiles')
+        install('wget')
+        print('Installed dependencies!')
+        time.sleep(3)
+
+    if choice == '4':
+        os.system(clear)
+        print('The following operations require administrator access.')
+        print('If you are not running in an administrative terminal/sudo, exit the program now.')
+        input('Press enter to continue.')
+        os.system(clear)
+        print('Installing discord.py, aiofiles, cffi, pycparser, PyNaCl, youtube_dl, wget and requirements...')
+        install('discord.py')
+        install('aiofiles')
+        install('PyNaCl')
+        install('youtube_dl')
+        install('wget')
+        install('cffi')
+        install('pycparser')
+        print('Installed python dependencies!')
+        time.sleep(1)
+        print('Installing ffmpeg...')
+        if useros == 'Windows':
+            if platform.machine().endswith('64'):
+                wget.download('https://www.videohelp.com/download/ffmpeg-3.3.2-win64-static.zip')
+                ffmpegzip = 'ffmpeg-3.3.2-win64-static.zip'
+            else:
+                wget.download('https://www.videohelp.com/download/ffmpeg-3.3.2-win32-static.zip')
+                ffmpegzip = 'ffmpeg-3.3.2-win32-static.zip'
+            with zipfile.ZipFile(ffmpegzip) as zip_ref:
+                for file in zip_ref.namelist():
+                    if file.startswith('bin'):
+                        zip_ref.extract(file, 'master')
+            ffmpegpath = os.getcwd() + '\master\bin'
+            os.system('setx /M PATH="' + ffmpegpath + ';%PATH%"')
+        elif useros == 'Linux':
+            os.system('add-apt-repository ppa:mc3man/trusty-media')
+            os.system('apt-get update')
+            os.system('apt-get install ffmpeg')
+            os.system('apt-get install frei0r-plugins')
+        elif useros == 'Darwin':
+            print('Mac Installation requires that you currently have homebrew installed.')
+            print('If homebrew is not installed, please close the program and install it.')
+            input('Press enter to continue.')
+            os.system('brew install ffmpeg --with-fdk-aac --with-ffplay --with-freetype --with-frei0r --with-libass --with-libvo-aacenc --with-libvorbis --with-libvpx --with-opencore-amr --with-openjpeg --with-opus --with-rtmpdump --with-schroedinger --with-speex --with-theora --with-tools')
+        print('Installed all depedencies!')
+
+    if choice == '5':
+        os.system(clear)
+        tokendump = input("Enter your bot's token here: ")
+        os.system(clear)
+        clientdump = input("Enter your bot's client secret here: ")
+        os.system(clear)
+        ownerdump = input("Enter your user id here: ")
+        os.system(clear)
+        serverdump = input("Enter your server id here: ")
+        os.system(clear)
+        moddump = input("Enter the name of your mod role here: ")
+        os.system(clear)
+        admindump = input("Enter the name of your admin role here: ")
+        data = {
+            "token":tokendump,
+            "client":clientdump
+        }
+        perms = {
+            "ownerid":ownerdump,
+            "serverid":serverdump,
+            "modrole":moddump,
+            "adminrole":admindump
+        }
+        os.chdir('master')
+        with open('config.json', 'w') as f:
+            json.dump(data, f)
+        with open('permissions.json', 'w') as p:
+            json.dump(perms, p)
+        os.chdir('..')
+        time.sleep(1)
+
+    if choice == '6':
+        sys.exit()
+
+    if choice == '7':
+        try:
+            wget.download('https://github.com/ANT1H3R0/osmibot/archive/master.zip', 'master.zip')
+            print('Downloaded')
+            time.sleep(2)
+            with zipfile.ZipFile('master.zip', 'r') as zip_ref:
+                zip_ref.extractall()
+            os.rename('osmibot-master', 'master')
+            os.remove('master.zip')
+        except Exception as e:
+            print('Error!')
+            print(e)
+            input('Press enter to continue...')
+
+    if choice == '8':
+        os.chdir('master')
+        os.system('python cogloader.py')
+        os.chdir('..')
+
+    if choice == '1969':
+        os.system(clear)
+        print('Commencing Countdown, engines on...')
+        time.sleep(2)
